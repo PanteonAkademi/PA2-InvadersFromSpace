49a11e882a7500bc44b3b24adb996fb8cfc5ac4a
bariserdem <bariserdem@panteon.games>
2023-07-03 21:29:03 +0300

    Bullet and Player Control
    Bullet and Player Control

---
+++ Assets/Scripts/Utilty/DestroyAfterSeconds.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour
+{
+    public float seconds;
+    void Start()
+    {
+        Destroy(gameObject, seconds);
+    }
+}
