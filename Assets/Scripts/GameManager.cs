dcf6cf36eb8eb3dbb93471bd89e43215c60450e4
bariserdem <bariserdem@panteon.games>
2023-07-19 20:56:09 +0300

    Added GameManager Scrippt
    Added GameManager Scrippt

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] allAlienSets;
+    private GameObject currentSet;
+    private Vector2 spawnPos = new Vector2(0, 10);
+    
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        SpawnWave();
+    }
+
+    public static void SpawnNewWave()
+    {
+        instance.StartCoroutine(instance.SpawnWave());
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        if(currentSet != null)
+            Destroy(currentSet);
+        yield return new WaitForSeconds(3);
+        currentSet = Instantiate(allAlienSets[Random.Range(0, allAlienSets.Length)],spawnPos, Quaternion.identity);
+        UIManager.UpdateWave();
+    }
+
+}
