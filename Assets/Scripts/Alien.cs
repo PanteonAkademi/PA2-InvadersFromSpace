4405f5b33e6f9eb03be1e18aeb39b57af92d9d8c
bariserdem <bariserdem@panteon.games>
2023-07-24 21:13:11 +0300

    Added Pickup Chance
    Added Pickup Chance

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Alien : MonoBehaviour
 {
     public int scoreValue;
     public GameObject explosion;
-    
+    public GameObject coinPrefab, lifePrefab, healthPrefab;
+
+    private const int LIFE_CHANCE = 1;
+    private const int HEALT_CHANCE = 10;
+    private const int COIN_CHANCE = 500;
 
     public void Kill()
     {
         UIManager.UpdateScore(scoreValue);
         AlienMaster.allAliens.Remove(gameObject);
         Instantiate(explosion, transform.position, Quaternion.identity);
-        
+
+        int ran = Random.Range(0, 1000);
+
+        if (ran <= LIFE_CHANCE)
+        {
+            Instantiate(lifePrefab, transform.position, Quaternion.identity);
+        }
+        else if (ran <= HEALT_CHANCE)
+        {
+            Instantiate(healthPrefab, transform.position, Quaternion.identity);
+        }else if (ran <= COIN_CHANCE)
+        {
+            Instantiate(coinPrefab, transform.position, Quaternion.identity);
+        }
 
         if (AlienMaster.allAliens.Count == 0)
         {
